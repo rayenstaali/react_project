8bf2fc4107302b56b79ab94f64dead4ae904d4bc
rayenstaali <52163867+rayenstaali@users.noreply.github.com>
2025-04-26 17:16:19 +0100

    Create categories.ts

---
+++ categories.ts
@@ -0,0 +1,82 @@
+export const categories = [
+  {
+    id: 'outils',
+    name: 'Outils',
+    image: 'https://images.pexels.com/photos/8105118/pexels-photo-8105118.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'outils-manuels', name: 'Outils manuels' },
+      { id: 'outils-electriques', name: 'Outils électriques' },
+      { id: 'outils-jardin', name: 'Outils de jardin' },
+    ]
+  },
+  {
+    id: 'quincaillerie',
+    name: 'Quincaillerie',
+    image: 'https://images.pexels.com/photos/4483608/pexels-photo-4483608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'vis-clous', name: 'Vis et clous' },
+      { id: 'serrurerie', name: 'Serrurerie' },
+      { id: 'fixations', name: 'Fixations' },
+    ]
+  },
+  {
+    id: 'peinture',
+    name: 'Peinture',
+    image: 'https://images.pexels.com/photos/5691635/pexels-photo-5691635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'peinture-interieur', name: 'Peinture intérieure' },
+      { id: 'peinture-exterieur', name: 'Peinture extérieure' },
+      { id: 'accessoires-peinture', name: 'Accessoires de peinture' },
+    ]
+  },
+  {
+    id: 'electricite',
+    name: 'Électricité',
+    image: 'https://images.pexels.com/photos/3825585/pexels-photo-3825585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'cables', name: 'Câbles' },
+      { id: 'interrupteurs', name: 'Interrupteurs et prises' },
+      { id: 'luminaires', name: 'Luminaires' },
+    ]
+  },
+  {
+    id: 'plomberie',
+    name: 'Plomberie',
+    image: 'https://images.pexels.com/photos/6419169/pexels-photo-6419169.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'robinetterie', name: 'Robinetterie' },
+      { id: 'tuyaux', name: 'Tuyaux et raccords' },
+      { id: 'evacuations', name: 'Évacuations' },
+    ]
+  },
+  {
+    id: 'jardin',
+    name: 'Jardin',
+    image: 'https://images.pexels.com/photos/1105019/pexels-photo-1105019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'mobilier-jardin', name: 'Mobilier de jardin' },
+      { id: 'arrosage', name: 'Arrosage' },
+      { id: 'amenagement', name: 'Aménagement paysager' },
+    ]
+  },
+  {
+    id: 'materiaux',
+    name: 'Matériaux',
+    image: 'https://images.pexels.com/photos/544966/pexels-photo-544966.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'bois', name: 'Bois' },
+      { id: 'ciment', name: 'Ciment et mortier' },
+      { id: 'isolation', name: 'Isolation' },
+    ]
+  },
+  {
+    id: 'decoration',
+    name: 'Décoration',
+    image: 'https://images.pexels.com/photos/3144580/pexels-photo-3144580.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    subcategories: [
+      { id: 'meubles', name: 'Meubles' },
+      { id: 'luminaires-deco', name: 'Luminaires' },
+      { id: 'accessoires-deco', name: 'Accessoires déco' },
+    ]
+  },
+];
